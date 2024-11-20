d409014ebf38f6c34c72cb037f76a90021991341
Nathan Holmquist <nholmquist2005@aol.com>
2024-11-20 16:40:21 -0500

    Initial Commit, PED#4, (midway thru video)

---
+++ bullshit.py
@@ -0,0 +1,10 @@
+
+temp = int(input("What temperature should the ovan be set to? (Please provide a whole number) "))
+time = int(input("How long shouod it cook in the ovan for? (minutes) "))
+
+microwave_Time = temp * time
+
+
+print("That would be equivelent to " + str(microwave_Time)+ " seconds in the microwave")
+
+
